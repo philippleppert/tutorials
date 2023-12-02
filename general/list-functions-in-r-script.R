9fb6770ab98e10b717ec1a348ecb227bfc7fbce9
Philipp Leppert <philippleppert@web.de>
2023-12-02 23:21:09 +0100

    Function to parse .Rmd file and then list all functions in corresponding R-script

---
+++ general/list-functions-in-r-script.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+
+root <- "linear-regression-2/"
+
+rmd_path <- paste0(root,"linear-regression-2.Rmd")
+
+knitr::purl(rmd_path, output = "general/linear-regression-2.R")
+
+r_path <- paste0(general, "linear-regression-2.R")
+
+NCmisc::list.functions.in.file(r_path, alphabetic = FALSE)
